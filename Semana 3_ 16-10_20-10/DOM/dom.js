c28f709cd21dab6a32804dc02f07bc77e9429f40
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-10-16 14:24:46 +0200

    Create dom.js

---
+++ Semana 3_ 16-10_20-10/DOM/dom.js
@@ -0,0 +1,19 @@
+/* console.log(window);
+ console.log(document);
+
+ let texto = "Hola soy Juan Carlos Sanchez y juego al futbol";
+ const hablar = (texto) =>
+   speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+
+ hablar(texto); 
+*/
+
+console.log("******** Elementos del Documento ********");
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.characterSet);
+console.log(document.title);
+console.log(document.links);
