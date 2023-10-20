c8b82949a9794d2d09973392d3389d803c3f82ea
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-10-20 10:31:51 +0200

    Create reloj_digital.js

---
+++ ejercicios-DOM/js/reloj_digital.js
@@ -0,0 +1,45 @@
+const d = document;
+
+export function digitalReloj(reloj, btnIniciar, btnParar) {
+  let relojTiempo;
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(btnIniciar)) {
+      relojTiempo = setInterval(() => {
+        let tiempo = new Date().toLocaleTimeString();
+        d.querySelector(reloj).innerHTML = `<h3> ${tiempo} </h3>`;
+      }, 1000);
+
+      e.target.disabled = true;
+    }
+
+    if (e.target.matches(btnParar)) {
+      clearInterval(relojTiempo);
+      d.querySelector(reloj).innerHTML = null;
+      d.querySelector(btnIniciar).disabled = false;
+    }
+  });
+}
+
+export function alarmaSonora(sound, alarmaSi, alarmaNo) {
+  let alarmaTempo;
+  const $alarma = d.createElement("audio");
+  $alarma.src = sound;
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(alarmaSi)) {
+      alarmaTempo = setTimeout(() => {
+        $alarma.play();
+      }, 2000);
+
+      e.target.disabled = true;
+    }
+
+    if (e.target.matches(alarmaNo)) {
+        clearTimeout(alarmaTempo)
+        $alarma.pause()
+        $alarma.currentTime = 0
+        d.querySelector(alarmaNo).disabled = false;
+    }
+  });
+}
