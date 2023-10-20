0250e843c606c2f3a906287a4a088c64d9b7a4df
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-10-20 10:31:25 +0200

    Update index_dom.js

--- ejercicios-DOM/js/index_dom.js
+++ ejercicios-DOM/js/index_dom.js
@@ -1,7 +1,10 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
+import {digitalReloj, alarmaSonora} from "./dom/reloj_digital.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel",".menu a");
+  digitalReloj("#reloj", "#activar-reloj", "#desactivar-reloj")
+  alarmaSonora("../../assets/soundAlarma.mp3", "#activar-alarma", "#desactivar-alarma")
 });
