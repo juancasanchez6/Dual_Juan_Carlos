3ebb4a516493eb630506d6e89bf3ccdb87276e35
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-10-25 11:08:16 +0000

    update-filtro_busquedas

--- ejercicios-DOM/js/filtro_busquedas.js
+++ ejercicios-DOM/js/filtro_busquedas.js
@@ -4,6 +4,9 @@ export default function filtrosBusqueda(input, selector) {
   d.addEventListener("keyup", (e) => {
     if (e.target.matches(input)) {
       //console.log(e.key);
+
+      if (e.key === "Escape") e.target.value = "";
+
       d.querySelectorAll(selector).forEach((el) =>
         el.textContent.toLowerCase().includes(e.target.value)
           ? el.classList.remove("filter")
