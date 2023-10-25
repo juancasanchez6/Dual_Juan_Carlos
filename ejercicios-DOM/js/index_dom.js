2e9f972d51a78189a699797c63e71a202c7ca33f
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-10-25 07:15:15 +0000

    update-deteccion_red

--- ejercicios-DOM/js/index_dom.js
+++ ejercicios-DOM/js/index_dom.js
@@ -7,6 +7,7 @@ import { darkTheme } from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
+import networkStatus from "./deteccion_red.js";
 
 const d = document;
 
@@ -42,8 +43,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     referrerpolicy="no-referrer-when-downgrade"></iframe>`
   );
 
-  responsiveTester("responsive-tester")
-  userDeviceInfo("user-device")
+  responsiveTester("responsive-tester");
+  userDeviceInfo("user-device");
 });
 
 d.addEventListener("keydown", (e) => {
@@ -52,3 +53,4 @@ d.addEventListener("keydown", (e) => {
 });
 
 darkTheme(".dark-theme-btn", "dark-mode");
+networkStatus();
