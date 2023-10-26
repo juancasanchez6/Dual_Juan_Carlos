a08122d9daef01a917c4d63e2a9c8fa6cef7fd43
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-10-26 18:30:30 +0000

    Update-validaciones-form

--- ejercicios-DOM/js/validaciones_formulario.js
+++ ejercicios-DOM/js/validaciones_formulario.js
@@ -22,19 +22,36 @@ export default function contactFormValidations() {
 
       if (pattern && $input.value !== "") {
         //console.log("El input tiene patron");
-        let regex = new RegExp(pattern)
+        let regex = new RegExp(pattern);
         return !regex.exec($input.value)
-        ?d.getElementById($input.name).classList.add("is-active")
-        :d.getElementById($input.name).classList.remove("is-active")
-        
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
       }
 
       if (!pattern) {
         //console.log("El input no tiene patron");
         return $input.value === ""
-        ?d.getElementById($input.name).classList.add("is-active")
-        :d.getElementById($input.name).classList.remove("is-active")
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
       }
     }
+  });
+
+  d.addEventListener("submit", (e) => {
+    //e.preventDefault();
+    alert("Enviando formulario");
+
+    const $loader = d.querySelector(".contact-form-loader"),
+      $response = d.querySelector(".contact-form-response");
+
+    $loader.classList.remove("none");
+
+    setTimeout(() => {
+      $loader.classList.add("none");
+      $response.classList.remove("none");
+      $form.reset();
+
+      setTimeout(() => $response.classList.add("none"), 3000);
+    }, 3000);
   });
 }
