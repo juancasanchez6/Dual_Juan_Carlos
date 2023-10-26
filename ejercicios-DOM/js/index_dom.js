ae9aa52037fd9349466a08a690c274707b0e5b3e
juancasanchez6 <131864347+juancasanchez6@users.noreply.github.com>
2023-10-26 17:30:27 +0000

    Update-validaciones-form

--- ejercicios-DOM/js/index_dom.js
+++ ejercicios-DOM/js/index_dom.js
@@ -15,6 +15,7 @@ import draw from "./sorteo.js";
 import slider from "./carrusel.js";
 import scrollSpy from "./scroll_espia.js";
 import smartVideo from "./video_inteligente.js";
+import contactFormValidations from "./validaciones_formulario.js";
 
 const d = document;
 
@@ -63,6 +64,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   slider();
   scrollSpy();
   smartVideo();
+  contactFormValidations();
 });
 
 d.addEventListener("keydown", (e) => {
